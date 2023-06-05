9a50e530fe44e9942d1821da7da0ba620215332f
Austin Schiller <auttie42496@yahoo.com>
2023-06-05 13:30:29 -0500

    trying to simplify JS

--- const generateMemeBtn = document.js
+++ const generateMemeBtn = document.js
@@ -3,6 +3,31 @@ const memeImage = document.querySelector(".meme-generator img");
 const memeTitle = document.querySelector(".meme-generator .meme-title");
 const memeAuthor = document.querySelector(".meme-generator .meme-author");
 
+async function getData(){
+    const url1 = ("https://meme-api.com/gimme")
+    const url2 = ("https://meme-api.com/gimme")
+
+    const response1 = await fetch(url1)
+    const data1 = await response1.json()
+
+    const response2 = await fetch(url2)
+    const data2 = await response2.json()
+}
+
+//stuff im trying
+
+class generateMemeBtn{
+    constructor(.meme-generator .generate-meme-btn)
+
+//Old and clunky
+
+ 
+ const generateMemeBtn = document.querySelector(".meme-generator .generate-meme-btn");
+ const memeImage = document.querySelector(".meme-generator img");
+ const memeTitle = document.querySelector(".meme-generator .meme-title");
+ const memeAuthor = document.querySelector(".meme-generator .meme-author");
+ 
+
 const generateMemeBtn2 = document.querySelector(".meme-generator-2 .generate-meme-btn-2");
 const memeImage2 = document.querySelector(".meme-generator-2 img");
 const memeTitle2 = document.querySelector(".meme-generator-2 .meme-title-2");
@@ -34,10 +59,4 @@ async function generateMeme2 (){
     const response = await fetch(url)
     const data2 = await response.json()
     updateDetails2(data2.url, data2.title, data2.author);
-}
-
-generateMemeBtn.addEventListener("click", generateMeme2);
-generateMemeBtn2.addEventListener("click", generateMeme);
-
-generateMeme();
-generateMeme2();
+} */
